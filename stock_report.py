daeb66da1cc4def083a9723c70af8f89944128d3
acikabot <alexdzam4@gmail.com>
2026-03-30 08:13:57 +0200

    Update stock_report.py

--- stock_report.py
+++ stock_report.py
@@ -15,7 +15,8 @@ GROQ_API_KEY       = os.environ.get("GROQ_API_KEY")
 FINNHUB_API_KEY    = os.environ.get("FINNHUB_API_KEY")
 EMAIL_SENDER       = os.environ.get("EMAIL_SENDER")
 EMAIL_PASSWORD     = os.environ.get("EMAIL_PASSWORD")
-EMAIL_RECEIVER     = "arotondi@airamcapital.com"
+EMAIL_RECEIVER     = ["arotondi@airamcapital.com", "aistockreports@gmail.com"]
+"
 
 
 # ─────────────────────────────────────────
